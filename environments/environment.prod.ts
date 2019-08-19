965380ed95c37a29bcfaff533e58097f2771f824
Ashwin Salhotra <ashwinsalhotra@users.noreply.github.com>
2019-08-19 11:03:38 +0100

    deploy21

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,3 +1,11 @@
+// environment.ts
 export const environment = {
-  production: true
-};
+  production: false,
+  auth: {
+    clientID: 'YOUR-AUTH0-CLIENT-ID',
+    domain: 'YOUR-AUTH0-DOMAIN', // e.g., https://you.auth0.com/
+    audience: 'YOUR-AUTH0-API-IDENTIFIER', // e.g., http://localhost:8100
+    redirect: 'http://localhost:8100/callback',
+    scope: 'openid profile email'
+  }
+}
